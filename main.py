8335cf1805810e1b6ea67ef59d2ff4d95bbc74e5
paulomatheuz <paulo.okkotsu@gmail.com>
2026-08-26 15:21:32 -0400

    feat: improve GitHub username validation

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-username = input("Digite um usuario do GitHub: ").strip()
+def hifen_nas_bordas(username):
+    if username == "":
+        return False
+
+    if username[0] == "-" or username[-1] == "-":
+        return True
+    else:
+        return False
+
 
 def username_valido(username):
-    if len(username) < 3:
+    if username == "" or len(username) > 39 or hifen_nas_bordas(username) or "--" in username:
         return False
     else:
         return True
 
+
+username = input("Digite um usuario do GitHub: ").strip()
+
 user = username_valido(username)
+
 if user:
-	print("Username recebido: " + username)
+    print("Username recebido: " + username)
 else:
-	print("Username invalido")
+    print("Username invalido")
